e5c07c77e5493fd0b468b540d301145d0e5a285a
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-28 14:43:11 +0900

    fix: 설정파일 수정, 주입 불발 예외 발생

--- src/main/java/com/example/cicdpractice/WebController.java
+++ src/main/java/com/example/cicdpractice/WebController.java
@@ -1,5 +1,6 @@
 package com.example.cicdpractice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class WebController {
 
+    @Value("${custom.setting}")
+    private String setting;
+
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @PostMapping("/setting")
+    public String setting(Model model) {
+        model.addAttribute("setting", setting);
         return "index";
     }
 
